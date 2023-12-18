3e403da9635ca657553e1b13dae8c8cc29b791a1
Prakhar Gupta <prakhar897@gmail.com>
2023-12-19 01:51:53 +0530

    Merging Improved code by @giesf (#1)
    * Improved load performance, seperated CSS into static file, cleaned up api
    
    ---------
    
    Co-authored-by: giesf <64958447+giesf@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Hacker News Comments Drawer (hn-comments-drawer)
 
-Integrate Hacker News comments and display them beautifully within your websites.
+Performance optimised version of the [Hacker News Drawer by prakhar897](https://github.com/prakhar897/hn-comments-drawer/)
 
 ## Demo
 
-[Live Demo 1](https://prakgupta.com/misc/hn-comments-drawer-demo)
+[New Performance](https://hn-comments-drawer.vercel.app/examples)
 
-[Live Demo 2](https://prakgupta.com/blog/moving_away_from_substack)
+[Old Performance](https://prakgupta.com/misc/hn-comments-drawer-demo)
+
 
 ![Screenshot of the product](https://raw.githubusercontent.com/prakhar897/hn-comments-drawer/main/screenshot.png)
 
@@ -20,13 +21,13 @@ Integrate Hacker News comments and display them beautifully within your websites
 
 1.  Include this snippet in your HTML document:
 
-    `<script src="https://cdn.jsdelivr.net/gh/prakhar897/hn-comments-drawer@main/src/script.js"></script>`
+    `<script src="https://hn-comments-drawer.vercel.app/src/script.js"></script>`
 
 2.  Create an HTML element where you want to display the comments. Use the following format for the ID:
 
         <div
-        	id="hn-comments-drawer storyId-38300167 maxDepth-3"
-        	class="hn-comments-drawer">
+            class="hn-comments-drawer"
+            hn-story-id="38300167">
         </div>
 
     Replace `38300167` with the actual ID of the Hacker News story for which you want to display comments.
@@ -36,25 +37,15 @@ Integrate Hacker News comments and display them beautifully within your websites
 -   Add maxDepth of comments:
 
         <div
-        	id="hn-comments-drawer
-        	storyId-38300167 maxDepth-3"
-        	class="hn-comments-drawer">
+            class="hn-comments-drawer"
+            hn-story-id="38300167"
+            hn-max-depth="30">
         </div>
 
 -   Add default styling:
 
-        <div
-        	id="hn-comments-drawer storyId-38300167 maxDepth-3 styling-default"
-        	class="hn-comments-drawer">
-        </div>
+       		<link rel="stylesheet" href="https://hn-comments-drawer.vercel.app/src/default.css"  />
 
-## JavaScript Integration
-
-    // Fetch and display Hacker News comments for a specific story ID
-
-    displayHNComments(storyId);
-
-Replace `storyId` with the actual ID of the Hacker News story for which you want to fetch comments.
 
 ## Styling
 
@@ -103,40 +94,7 @@ Customize the styling of the comments by modifying the provided CSS classes in y
 
 ## Examples
 
-    	<!DOCTYPE html>
-    	<html lang="en">
-    		<head>
-    			<meta charset="UTF-8" />
-    			<title>Hacker News Comments</title>
-    			<style>
-    				.hn-comments-drawer {
-    					border: 10px solid blue;
-    				}
-    			</style>
-    		</head>
-    		<body>
-    			<h1>Hacker News Comments</h1>
-
-    			<!-- Create multiple comment drawers with different story IDs -->
-    			<h2>Story 1 Comments</h2>
-    			<div
-    				id="hn-comments-drawer storyId-38300167 maxDepth-10 styling-default"
-    				class="hn-comments-drawer"
-    			></div>
-
-    			<h2>Story 2 Comments</h2>
-    			<div
-    				id="hn-comments-drawer storyId-38378455 maxDepth-10"
-    				class="hn-comments-drawer"
-    			></div>
-
-    			<!-- Automatically load comments -->
-    			<!-- <script src="../src/script.js"></script> -->
-    			<script src="https://cdn.jsdelivr.net/gh/prakhar897/hn-comments-drawer@main/src/script.js"></script>
-    		</body>
-    	</html>
-
-You can find more examples in `examples` folder.
+You can find examples in the `examples` folder.
 
 ## Contributing
 
--- src/script.js
+++ src/script.js
@@ -1,220 +1,114 @@
 // Create a function that fetches comments and displays them
 async function displayHNComments(
 	storyId,
-	commentsDrawer,
-	options = { maxDepth: 100, styling: null }
+	rootElement,
+	options
 ) {
-	try {
-		// Fetch story details from the Hacker News API
-		const storyResponse = await fetch(
-			`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
-		);
-		const storyData = await storyResponse.json();
+	try {	
+		rootElement.innerHTML = ""
 
-		if (!storyData.kids || storyData.kids.length === 0) {
-			commentsDrawer.innerHTML = "No comments found.";
+		const story = await fetchItemFromHN(storyId)
+		if (!story.kids || story.kids.length === 0) {
+			rootElement.innerHTML = "No comments found.";
 			return;
 		}
 
-		commentsDrawer.innerHTML += `
-    <div id="hn-comments-drawer-heading" class="hn-comments-drawer-heading">
-      Comments On this post :
-      <a href="https://news.ycombinator.com/item?id=${storyId}" target="_blank">
-        ${storyData.title}
-      </a>
-    </div>
-    `;
+		rootElement.innerHTML += renderCommentsHeader(story.id, story.title)
+		const commentsWrapper = rootElement.appendChild(document.createElement("ul"));
+		commentsWrapper.classList.add("comments-list")
+		commentsWrapper.style.marginRight = 0 * 20;
 
-		// Show loading spinner
-		commentsDrawer.innerHTML += `
-    <div class="comments-loading">
-      Comments Loading , Please wait...
-    </div>
-  `;
+		const commentIds = story.kids 
+		await createCommentListElement(commentIds,rootElement,0,options.maxDepth)
 
-		const comments = storyData.kids;
-		const commentsHTML = await createCommentsHTML(comments, options);
-
-		// Remove the loading spinner after commentsHTML has been created
-		const loadingSpinner =
-			commentsDrawer.querySelector(".comments-loading");
-		if (loadingSpinner) {
-			loadingSpinner.remove();
-		}
-
-		// Display comments in the comments drawer element
-		commentsDrawer.innerHTML += commentsHTML;
-
-		commentsDrawer.innerHTML += `<div id="hn-comments-drawer-footer" class="hn-comments-drawer-footer">
-    <span>This comment section is powered by </span><a href="https://github.com/prakhar897/hn-comments-drawer" target="_blank"> Hn-comments-drawer <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
-  </div>`;
-
-		// Add basic styles for comments dynamically
-		if (options.styling == "default") {
-			addDefaultStyles();
-		}
+		rootElement.innerHTML += renderCommentsFooter()
+		
 	} catch (error) {
 		console.error("Error fetching comments:", error);
 		commentsDrawer.innerHTML = "Error fetching comments.";
 	}
 }
 
-// Create a recursive function to generate HTML for comments
-async function createCommentsHTML(comments, options, depth = 0) {
-	let html = `<ul class="comments-list" style="margin-left: ${
-		depth * 20
-	}px;">`;
 
-	for (const commentId of comments) {
-		try {
-			const commentResponse = await fetch(
-				`https://hacker-news.firebaseio.com/v0/item/${commentId}.json`
-			);
-
-			const commentData = await commentResponse.json();
-
-			if (
-				commentData == null ||
-				(commentData.deleted && commentData.deleted == true)
-			) {
-				continue;
-			}
-
-			html += `<li>
-        <div class="comment-header">(${commentData.by})</div>
-        \n
-        <div class="comment-text">${commentData.text} </div>`;
-
-			if (
-				commentData.kids &&
-				commentData.kids.length !== 0 &&
-				depth < options.maxDepth
-			) {
-				html += await createCommentsHTML(
-					commentData.kids,
-					options,
-					depth + 1
-				);
-			}
-
-			html += `</li>`;
-		} catch (error) {
-			console.error(
-				"Error fetching comments:" +
-					error +
-					" for comment id:" +
-					commentId
-			);
-			html += `<li>Error fetching comments.</li>`;
-		}
-	}
-
-	html += "</ul>";
-
-	return html;
+async function fetchItemFromHN(itemId){
+		const res = await fetch(
+			`https://hacker-news.firebaseio.com/v0/item/${itemId}.json`
+		);
+		const item = await res.json();
+		return item;
 }
 
-// Function to add basic styles for comments dynamically
-function addDefaultStyles() {
-	// Create a style element
-	const styleElement = document.createElement("style");
+function renderCommentsHeader(storyId, title){
+	return `
+		<div id="hn-comments-drawer-heading" class="hn-comments-drawer-heading">
+		Comments On this post :
+		<a href="https://news.ycombinator.com/item?id=${storyId}" target="_blank">
+			${title}
+		</a>
+		</div>
+		`;
+}
+function renderCommentsFooter(){
+	return `<div id="hn-comments-drawer-footer" class="hn-comments-drawer-footer">
+		<span>This comment section is powered by </span><a href="https://github.com/prakhar897/hn-comments-drawer" target="_blank"> hn-comments-drawer ${renderGithubLogo()}</a>
+	</div>`;
+}
 
-	// Define your styles
-	const styles = `
-    
-    .hn-comments-drawer {
-      list-style-type: none;
-      margin-left: 0;
-      padding-left: 20px;
-      border: 2px solid red;
-      margin-top: 50px;
-      margin-bottom: 50px;
+async function createCommentListElement(commentIds,parentElement, depth, maxDepth = 10){
+	const commentsWrapper = parentElement.appendChild(document.createElement("ul"));
+	commentsWrapper.classList.add("comments-list")
+	commentsWrapper.style.marginRight = depth * 20;
 
-    }
+	console.log({maxDepth,depth})
+	if(depth >= maxDepth) return;
 
-    .hn-comments-drawer-heading{
-      color: red;
-    }
-
-    .comments-list {
-      list-style-type: none; /* Remove default list-style dots */
-      padding-left: 0; /* Remove default padding */
-    }
-    
-    .comments-list li {
-      border-left: 2px solid #ccc; /* Adjust thickness and color as needed */
-      padding-left: 10px; /* Adjust left padding for spacing */
-      margin-left: 5px; /* Adjust margin for spacing */
-      margin-top: 20px;
-    }
-
-    .comment-text {
-      color: red;
-    }
-
-    .comment-header {
-      color:blue;
-    }
-
-    .comment-text p {
-      color:blue;
-    }
-
-    .hn-comments-drawer-footer{
-      color: gray;
-    }
-
-	.comments-loading {
-		text-align: center;
-		padding: 2rem;
-	  }
-	  
-    
-  `;
-
-	// Set the styles within the style element
-	styleElement.innerHTML = styles;
-
-	const headElement = document.querySelector("head");
-	if (headElement) {
-		headElement.insertBefore(styleElement, headElement.firstChild);
-	} else {
-		console.error("<head> element not found.");
+	for(const commentId of commentIds){
+		await createCommentElement(commentId,commentsWrapper,depth+1,maxDepth);
 	}
 }
 
-// Handles Id to Attributes Conversion
-async function idToAttributes(id) {
-	const attributesList = id.split(" ");
-	const attributes = {};
+async function createCommentElement(commentId, parentElement, depth, maxDepth){
 
-	for (let i = 0; i < attributesList.length; i++) {
-		const attribute = attributesList[i];
-		const [key, value] = attribute.split("-");
-		if (key && value) {
-			if (key == "hn" && value == "comments") {
-				continue;
-			} else {
-				attributes[key] = isNaN(value) ? value : parseInt(value);
-			}
-		}
+	const comment = await fetchItemFromHN(commentId)
+
+	if(comment == null || comment.deleted) return false;
+
+	const commentElement = document.createElement("li");
+	commentElement.innerHTML = `
+	<div class="comment-header comment-author">${comment.by}</div>
+	<div class="comment-body comment-text">${comment.text}</div>
+	`
+	parentElement.appendChild(commentElement)
+
+	if(comment.kids && comment.kids.length > 0){
+		createCommentListElement(comment.kids,commentElement,depth+1,maxDepth)	
 	}
-
-	return attributes;
+	
 }
 
-// Automatically find and display Hacker News comments on load
+
+function renderGithubLogo(){
+	return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>`
+}
+
+
 document.addEventListener("DOMContentLoaded", function (event) {
 	const hnCommentsDrawers = document.querySelectorAll(
-		'[id^="hn-comments-drawer"]'
+		'[hn-story-id]'
 	);
 
 	hnCommentsDrawers.forEach(async (drawer) => {
-		const attributes = await idToAttributes(drawer.id);
 
-		displayHNComments(attributes.storyId, drawer, {
-			maxDepth: attributes.maxDepth,
-			styling: attributes.styling,
+		const attr = (attrName)=>drawer.getAttribute(attrName)
+		const storyId = attr("hn-story-id")
+
+
+		console.log( typeof  attr("hn-max-depth"))
+		const maxDepth = typeof attr("hn-max-depth") === "string" ? parseInt( attr("hn-max-depth")) : 10;
+
+		displayHNComments(storyId, drawer, {
+			maxDepth
+			
 		});
 	});
 });
